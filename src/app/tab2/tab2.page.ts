b6db4bcb5109ac382d94d813aec711a2e8b4975a
Melany Chechi <128297617+MelChechiS@users.noreply.github.com>
2023-03-22 22:34:41 -0300

    Finalização do Tab Truco
    Adicionando funções e melhoramento do Css

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,8 +8,84 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
- 
+  valendo = 1;
+  time1_score = 0;
+  time2_score = 0;
+  time1_wins = 0;
+  time2_wins = 0;
 
-  constructor() {}
+  constructor(private toastController: ToastController) { }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'TRUCO!',
+      duration: 1500,
+      position: position,
+      cssClass: 'toast'
+    });
+
+    await toast.present();
+  }
+
+  change_contador(contador: number) {
+    this.valendo = contador
+    if(this.valendo > 2){
+      this.presentToast('bottom')
+    }
+  }
+
+  increase_team1_score() {
+    this.time1_score += this.valendo
+    this.valendo = 1
+    if (this.time1_score >= 12) {
+      this.time1_wins += 1
+      this.time1_score = 0
+    }
+  }
+
+  decrease_team1_score() {
+    if (this.time1_score <= 0) {
+      // do nothing
+    } else {
+      this.time1_score -= this.valendo
+      this.valendo = 1
+    }
+
+    if(this.time1_score <= 0){
+      this.time1_score = 0
+
+    }
+  }
+
+  increase_team2_score() {
+    this.time2_score += this.valendo
+    this.valendo = 1
+    if (this.time2_score >= 12) {
+      this.time2_wins += 1
+      this.time2_score = 0
+    }
+  }
+
+  decrease_team2_score() {
+    if (this.time2_score <= 0) {
+      // do nothing
+    } else {
+      this.time2_score -= this.valendo
+      this.valendo = 1
+    }
+
+    if(this.time1_score <= 0){
+      this.time1_score = 0
+    }
+  }
+
+  clean() {
+    this.valendo = 1;
+    this.time1_score = 0;
+    this.time2_score = 0;
+    this.time1_wins = 0;
+    this.time2_wins = 0;
+  }
+
 
 }
